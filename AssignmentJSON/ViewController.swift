23f86551a0751e2fdda9eecb557b07022ef6baa8
Suraj <corpussai123@gmail.com>
2022-04-26 12:32:14 +0530

    Initial Commit

---
+++ AssignmentJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentJSON
+//
+//  Created by SAISURAJ on 4/26/22.
+//  Copyright © 2022 SAISURAJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
